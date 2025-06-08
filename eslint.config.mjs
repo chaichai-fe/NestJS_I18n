67cd00107c49a3b9f7658c90b3a46534c5837ac1
不会写React的柴柴 <75667054+chaichai-fe@users.noreply.github.com>
2025-06-08 23:10:01 +0800

    feat:update pm2 config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['ecosystem.config.js'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
