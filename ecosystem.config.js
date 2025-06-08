3a0ce36eb37a7bd62f749930f6f645b25884b039
不会写React的柴柴 <75667054+chaichai-fe@users.noreply.github.com>
2025-06-08 22:55:15 +0800

    feat:add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nestjs-i18n',
+      script: 'dist/src/main.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+      env_development: {
+        NODE_ENV: 'development',
+      },
+      error_file: 'logs/err.log',
+      out_file: 'logs/out.log',
+      log_file: 'logs/combined.log',
+      time: true,
+    },
+  ],
+}
