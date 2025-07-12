7adee42364d0bb58e60d9005e15cd24164bc6722
不会写React的柴柴 <75667054+chaichai-fe@users.noreply.github.com>
2025-07-12 17:56:11 +0800

    feat:add a default router

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { LangTagModule } from './langTag/langTag.module'
 import { BusinessTagModule } from './businessTag/businessTag.module'
 import { TranslationsModule } from './translations/translations.module'
 import { AuthModule } from './auth/auth.module'
-
+import { AppController } from './app.controller'
 @Module({
   imports: [LangTagModule, BusinessTagModule, TranslationsModule, AuthModule],
+  controllers: [AppController],
 })
 export class AppModule {}
